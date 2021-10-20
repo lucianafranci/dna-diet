d96e993aec4b28cd690bc5da7783216b3f48c603
MATT-HP\matia <matias.franci@gmail.com>
2021-10-19 20:22:26 -0500

    refactoring

--- rosalind-problems/rabbit.py
+++
@@ -1,9 +0,0 @@
-def fibonacci_loop_child(months, offsprings):
-    # offsprings = descendencia
-    parrent, child = 1, 1
-    for itr in range(months - 1):
-        child, parrent = parrent, parrent + (child * offsprings)
-    return child
-
-
-print(fibonacci_loop_child(5, 2))
